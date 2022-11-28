ec33b7c4f0438dc46a2749e78f13dc8a783eaace
henrietta jeansson <henriettajeansson@gmail.com>
2022-11-28 16:26:59 +0100

    added emma as a person class

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,10 @@
+import { Person } from "./models/person";
+
 console.log("Hallå och välkommen till att se våran reda med git!");
+
+let Emma = new Person (){
+    (age: 28,
+    name: Emma,
+    height: 170)
+
+}
--- src/ts/models/person.ts
+++ src/ts/models/person.ts
@@ -3,6 +3,9 @@ export class Person {
     name!: string;
     height!: number;
     constructor(age: number, name: string, height: number){
+        this.age = age;
+        this.name = name;
+        this.height = height;
     };
         greeting(): string{
             return "Hej halloj!";
