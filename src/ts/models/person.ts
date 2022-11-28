a2eee2f3fdcb3c8f30cde1ae363c131559333763
henrietta jeansson <henriettajeansson@gmail.com>
2022-11-28 16:18:01 +0100

    added models with class

---
+++ src/ts/models/person.ts
@@ -0,0 +1,12 @@
+export class Person {
+    age!: number;
+    name!: string;
+    height!: number;
+    constructor(age: number, name: string, height: number){
+    };
+        greeting(): string{
+            return "Hej halloj!";
+
+        }
+    
+}
