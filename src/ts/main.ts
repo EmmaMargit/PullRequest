f0599845f2d3da1b863a33674c78f5b8172448a0
henrietta jeansson <henriettajeansson@gmail.com>
2022-11-28 16:28:37 +0100

    class emma no error

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,9 +2,7 @@ import { Person } from "./models/person";
 
 console.log("Hallå och välkommen till att se våran reda med git!");
 
-let Emma = new Person (){
-    (age: 28,
-    name: Emma,
-    height: 170)
+let Emma = new Person (28, "Emma", 170);{
+    
 
 }
